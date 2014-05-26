47981a3dc07d03c8e07be5d50afb18ad57b51ac6
andreCartezani <andre.mcart@gmail.com>
2014-05-26 01:38:51 +0100

    primeira semana

---
+++ porra.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int num, float met char const *argv[])
+{
+	printf("digite um numero qualquer, fila da puta.: ");
+	scanf("%i, &num");
+	met=num/2;
+	printf("a metade do numero do caralho eh %f", met)
+
+	getch();
+}
